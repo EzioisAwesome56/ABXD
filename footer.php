2569295c71b256b02009ba4fd0d67452504e00b6
EzioisAwesome56 <jedijon567@gmail.com>
2017-02-28 22:09:29 -0500

    edited version info

--- footer.php
+++ footer.php
@@ -5,9 +5,9 @@ else echo '<a href="#" onclick="enableMobileLayout(-1); return false;" rel="nofo
 <br>
 <br>
 <?php $bucket = "footer"; include("./lib/pluginloader.php");?>
-Powered by <a href="http://abxd.dirbaio.net/">AcmlmBoard XD</a> 3.0<br />
-By Dirbaio, xfix, Kawa, StapleButter, Nina, et al<br />
-AcmlmBoard &copy; Jean-Fran&ccedil;ois Lapointe<br />
+Powered by <a href="https://github.com/EzioisAwesome56/Acmlmboard-XD-Jon">Jon's AcmlmBoard XD</a><br />
+By EzioisAwesome56<br />
+AcmlmBoard XD by Dirbaio, xfix, Kawa, StapleButter, Nina, et al<br />
 <?php print __("<!-- English translation by The ABXD Team -->")?>
 
 <?php print (isset($footerButtons) ? $footerButtons : "")?>
