13aed0769811c71bb125630e70f479741786651e
EzioisAwesome56 <jedijon567@gmail.com>
2017-03-01 19:36:35 -0500

    tweak the default text

--- plugins/homepage/settingsfile.php
+++ plugins/homepage/settingsfile.php
@@ -2,7 +2,7 @@
 	$settings = array(
 		"homepage" => array (
 			"type" => "textbbcode",
-			"default" => "Hello, this is some test thing",
+			"default" => "<h1>Welcome to Jon's Acmlmboard XD!</h1><br>To install new features, please see the plugin manager",
 			"name" => "Hompage Text"
 		),
 	);
